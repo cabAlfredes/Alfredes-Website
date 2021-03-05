3307b7dcaa107d1bc49e0043ffd3cfecab951ee8
Bernardo <moonbe77@gmail.com>
2021-03-06 09:40:56 +1100

    Hero plus video

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function contact(props) {
+  return <div>Contact</div>;
+}
+
+export default contact;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,17 @@
 /* eslint-disable import/no-unresolved */
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
-import Logo from '@/components/Logo';
+import Hero from '@/components/Hero';
+// import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-  const handlePause = () => {
-    document.getElementById('video');
-  };
-
   return (
     <>
       <Head>
         <title>Cabaña Alfredes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.videoWrapper}>
-        {/* <!-- The video --> */}
-        <video autoPlay muted loop id="video" className={styles.video}>
-          <source src="/cab-video_Trim2.mp4" type="video/mp4" />
-        </video>
-        <div className={styles.controls}>
-          <button type="button" onClick={handlePause}>
-            pause
-          </button>
-        </div>
-
-        <div className={styles.content}>
-          <Logo className={styles.logo} color="red" />
-        </div>
+      <div>
+        <Hero />
       </div>
     </>
   );
