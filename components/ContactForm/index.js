a2280f2a0f74a69625c7ab70db99d2ea8a52fefc
Bernardo <moonbe77@gmail.com>
2021-03-08 17:23:28 +1100

    Form box with background

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -20,61 +20,63 @@ function ContactForm(props) {
 
   console.log(formData);
   return (
-    <div className={styles.formWrapper}>
-      <h1>Consultas</h1>
-      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-        <input
-          onChange={handleChange}
-          type="text"
-          name="name"
-          className={styles.input}
-          placeholder="nombre"
-          required
-        />
-        <input
-          onChange={handleChange}
-          type="email"
-          name="email"
-          className={styles.input}
-          placeholder="email@tuemail.com"
-          required
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          name="phone"
-          className={styles.input}
-          placeholder="teléfono"
-          required
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          name="dateFrom"
-          className={styles.input}
-          placeholder="desde"
-        />
-        <input
-          onChange={handleChange}
-          type="text"
-          name="dateTo"
-          className={styles.input}
-          placeholder="hasta"
-        />
+    <div className={styles.contact}>
+      <div className={styles.formWrapper}>
+        <h1>Consultas</h1>
+        <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="name"
+            className={styles.input}
+            placeholder="nombre"
+            required
+          />
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            className={styles.input}
+            placeholder="email@tuemail.com"
+            required
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="phone"
+            className={styles.input}
+            placeholder="teléfono"
+            required
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="dateFrom"
+            className={styles.input}
+            placeholder="desde"
+          />
+          <input
+            onChange={handleChange}
+            type="text"
+            name="dateTo"
+            className={styles.input}
+            placeholder="hasta"
+          />
 
-        <textarea
-          onChange={handleChange}
-          className={styles.textarea}
-          name="message"
-          id="message"
-          cols="80"
-          rows="10"
-          placeholder="mensaje"
-        />
-        <input type="submit" />
-        <input type="reset" />
-      </form>
-      <div className={styles.message}>Message</div>
+          <textarea
+            onChange={handleChange}
+            className={styles.textarea}
+            name="message"
+            id="message"
+            cols="80"
+            rows="10"
+            placeholder="mensaje"
+          />
+          <input type="submit" />
+          <input type="reset" />
+        </form>
+        <div className={styles.message}>Message</div>
+      </div>
     </div>
   );
 }
