001bfe50a68c67dfbbac0ddfa5d11dc6a96ec2e7
Bernardo <moonbe77@gmail.com>
2021-04-21 16:13:31 +1000

    base to apply context state

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,53 +1,59 @@
 import Link from 'next/link';
+import { useStateContext, useStateDispatch } from '@/store/store';
 import styles from './Menu.module.scss';
 
-const Menu = () => (
-  <nav className={styles.nav}>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <a
-          href="/#about"
-          onClick={(e) => {
-            e.preventDefault(); // prevent the page to move to top if click more than once the link
-            window.location = '/#about';
-          }}
-        >
-          La Cabana
-        </a>
-      </li>
-      <li>
-        <a
-          href="/#services"
-          onClick={(e) => {
-            e.preventDefault(); // prevent the page to move to top if click more than once the link
-            window.location = '/#services';
-          }}
-        >
-          Servicios
-        </a>
-      </li>
-      <li>
-        <Link href="/gallery">
-          <a>Fotos</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/map">
-          <a>Como Llegar</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact">
-          <a>Contacto</a>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Menu = () => {
+  const state = useStateContext();
+  const showList = true ? styles.visible : styles.hidden;
+
+  return (
+    <nav className={`${styles.nav} ${showList}`}>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <a
+            href="/#about"
+            onClick={(e) => {
+              e.preventDefault(); // prevent the page to move to top if click more than once the link
+              window.location = '/#about';
+            }}
+          >
+            La Cabana
+          </a>
+        </li>
+        <li>
+          <a
+            href="/#services"
+            onClick={(e) => {
+              e.preventDefault(); // prevent the page to move to top if click more than once the link
+              window.location = '/#services';
+            }}
+          >
+            Servicios
+          </a>
+        </li>
+        <li>
+          <Link href="/gallery">
+            <a>Fotos</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/map">
+            <a>Como Llegar</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contacto</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
