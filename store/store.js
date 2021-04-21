53071b3b6f81413208f9674495ad1d676c62483a
Bernardo <moonbe77@gmail.com>
2021-04-21 16:07:57 +1000

    FET: add burguer menu

---
+++ store/store.js
@@ -0,0 +1,30 @@
+import { useReducer, createContext, useContext } from 'react';
+
+export const StateContext = createContext();
+const StateDispatcher = createContext();
+const initialState = {
+  showMenu: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_MENU':
+      return { ...state, showMenu: action.payload };
+
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+};
+
+export const StateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <StateDispatcher.Provider value={dispatch}>
+      <StateContext.Provider value={state}>{children}</StateContext.Provider>
+    </StateDispatcher.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
+export const useStateDispatch = () => useContext(StateDispatcher);
