f69c0feaff9c7d180b19110e8e7fe290fedd1b1c
Bernardo <moonbe77@gmail.com>
2021-09-27 00:14:46 +1000

    add theme refactor contact

--- theme/theme.js
+++ theme/theme.js
@@ -1,49 +1,96 @@
 // import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
-
-export const themeOptions = {
-  mode:'light',
+export const lightTheme = {
   palette: {
     type: 'light',
     primary: {
-      main: '#fdd835',
+      main: '#ffd600',
     },
     secondary: {
       main: '#f50057',
     },
   },
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#388e3c',
+  typography: {
+    h1: {
+      fontSize: '5rem',
+      fontWeight: 300,
     },
-    secondary: {
-      main: '#f50057',
+  },
+  spacing: 8,
+  overrides: {
+    MuiButton: {
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+      },
+    },
+    MuiLink:{
+      textDecoration: 'none'
+    }
+  },
+  props: {
+    MuiList: {
+      dense: true,
+    },
+    MuiMenuItem: {
+      dense: true,
+    },
+    MuiTable: {
+      size: 'small',
     },
   },
 };
 
+export const darkTheme = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#ffd600',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '5rem',
+      fontWeight: 300,
+    },
+  },
+  spacing: 8,
+  overrides: {
+    MuiButton: {
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+      },
+    },
+  },
+  props: {
+    MuiList: {
+      dense: true,
+    },
+    MuiMenuItem: {
+      dense: true,
+    },
+    MuiTable: {
+      size: 'small',
+    },
+  },
+};
 
 export const getDesignTokens = (mode) => ({
   palette: {
     mode,
-    ...(mode === 'light'
-      ? {
-          // palette values for light mode
-          primary: {
-            main: '#fdd835',
-          },
-          secondary: {
-            main: '#f50057',
-          },        
-        }
-      : {
-          // palette values for dark mode
-          primary: {
-            main: '#388e3c',
-          },
-          secondary: {
-            main: '#f50057',
-          },         
-        }),
+    ...(mode === 'light'  ? {...lightTheme} : {...darkTheme})
   },
 });
