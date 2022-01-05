40f31ca9843ed56620cd6f5c2ba60860c2214e96
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2022-01-05 13:41:42 +1100

    add images to context and img comp resp wrapper

--- pages/fotos.js
+++ pages/fotos.js
@@ -1,30 +1,36 @@
-// import ContactForm from '@/components/ContactForm';
+
 import Head from 'next/head';
+import { useStateContext } from '@/store/store';
 import Image from 'next/image';
 import { styled } from '@mui/system';
 import Slider from '@/components/Slider';
 import { FullWidthPanel } from '@/components/Layout/templates';
 import { Box } from '@mui/system';
+//import context
 
 const ImageList = styled('div')({
-  display: 'flex',
-  gap: '10px',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'red',
+  columnCount: 3,
+  columnGap: 10,
+  display: 'block',
+  listStyleType: 'none',
+  overflowY: 'auto',
+  padding: 0,
+  margin: 0,
+  border: '1px solid red',
 })
 
 const ImageListItem = styled('div')({
   position: 'relative',
-  display: 'inline-block',
+  // display: 'inline-block',
   lineHeight: 0,
   height: 'auto',
   marginBottom: 10
 })
 
+
 function Gallery(props) {
+  const { images } = useStateContext();
+  console.log('images >>;)>>', images);
   return (
     <>
       <Head>
@@ -35,16 +41,52 @@ function Gallery(props) {
       <div>
         <h1>Fotos de la Cabaña</h1>
         <h3>Interior</h3>
+        <ImageList>
+          {images.interior.map((image, index) => (
+            <ImageListItem key={index}>
+              <Box
+                sx={{
+                  display: 'inline-flex',
+                  paddingTop: `${100 / (image.width / image.height)}%`,
+                }}
+              >
+                <Box sx={{ position: 'absolute', inset: 0, margin: 0 }} as={'figure'}>
+                  <Image
+                    src={image.src}
+                    alt={image.alt || "Foto"}
+                    // blurDataURL={image.blurDataURL}
+                    // placeholder={'blur'}
+                    layout={'fill'}
+                    objectFit={'cover'}
+                  />
+                </Box>
+              </Box>
+            </ImageListItem>
+          ))}
+        </ImageList>
         <h3>Exterior</h3>
         <ImageList>
-          <Image src='/assets/images/foto_1.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_2.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_3.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_4.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_5.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_6.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_7.JPG' width='300px' height='300px' />
-          <Image src='/assets/images/foto_8.JPG' width='300px' height='300px' />
+          {images.exterior.map((image, index) => (
+            <ImageListItem key={index}>
+              <Box
+                sx={{
+                  display: 'inline-flex',
+                  pt: `${100 / (image.width / image.height)}%`
+                }}
+              >
+                <Box sx={{ position: 'absolute', inset: 0, m: 0 }} as={'figure'}>
+                  <Image
+                    src={image.src}
+                    alt={image.alt || "Foto"}
+                    // blurDataURL={image.blurDataURL}
+                    placeholder={'blur'}
+                    layout={'fill'}
+                    objectFit={'cover'}
+                  />
+                </Box>
+              </Box>
+            </ImageListItem>
+          ))}
         </ImageList>
       </div>
     </>
