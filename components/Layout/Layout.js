b1934806c20a670cb32893e679f4f685386d2c7d
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2022-01-07 13:09:37 +1100

    improved palette, add formik to contact form, pages title banner

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,5 @@
-/** @format */
-import PropTypes from 'prop-types';
-import { useTransition, animated } from 'react-spring';
-import { useRouter } from 'next/router';
 import Header from '../Header/Header';
 import styles from './layout.module.scss';
-import { Container, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 
 const Footer = () => (
@@ -17,24 +12,6 @@ const Footer = () => (
 );
 
 function Layout({ children }) {
-  const Router = useRouter();
-  const transitions = useTransition(Router, {
-    unique: true,
-    reset: true,
-    from: {
-      opacity: 0,
-      transform: Router.pathname !== '/' && 'translate3d(0,-20px,0)',
-    },
-    enter: {
-      opacity: 1,
-      transform: Router.pathname !== '/' && 'translate3d(0, 0px,0)',
-    },
-    leave: {
-      opacity: 0,
-      transform: Router.pathname !== '/' && 'translate3d(0,-40px,0)',
-    },
-  });
-
   return (
     <Box
       sx={{
@@ -51,10 +28,6 @@ function Layout({ children }) {
           py: 3,
           px: 2,
           mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
         }}
       >
         <Footer />
