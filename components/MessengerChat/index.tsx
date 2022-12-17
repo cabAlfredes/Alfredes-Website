73a328335ff29f070b63fee2ed76602a8991807c
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2022-12-18 10:41:51 +1100

    comment lint error on chat component

--- components/MessengerChat/index.tsx
+++ components/MessengerChat/index.tsx
@@ -1,22 +1,22 @@
 export const init = () => {
-  // var chatbox = document.getElementById("fb-customer-chat");
-  // chatbox.setAttribute("page_id", "239506916203358");
-  // chatbox.setAttribute("attribution", "biz_inbox");
+//   // var chatbox = document.getElementById("fb-customer-chat");
+//   // chatbox.setAttribute("page_id", "239506916203358");
+//   // chatbox.setAttribute("attribution", "biz_inbox");
 
-  window.fbAsyncInit = (function () {
-    window.FB.init({
-      xfbml: true,
-      version: "v14.0",
-    });
-  })(
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk")
-  );
+//   window.fbAsyncInit = (function () {
+//     window.FB.init({
+//       xfbml: true,
+//       version: "v14.0",
+//     });
+//   })(
+//     (function (d, s, id) {
+//       var js,
+//         fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//       js = d.createElement(s);
+//       js.id = id;
+//       js.src = "https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js";
+//       fjs.parentNode.insertBefore(js, fjs);
+//     })(document, "script", "facebook-jssdk")
+//   );
 };
