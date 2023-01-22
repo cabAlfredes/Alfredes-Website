fd21aac9345d0e8f42074d41c3f7054a01b0dc80
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2023-01-22 13:11:44 +1100

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 /** @format */
-// import type {} from '@mui/x-date-pickers/themeAugmentation';
+import Script from "next/script";
 import Layout from "../components/Layout/Layout";
 import { StateProvider } from "../store/store";
 import { SiteThemeProvider } from "../theme/theme-provider";
 import CssBaseline from "@mui/material/CssBaseline";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import "../styles/globals.css";
 
@@ -13,23 +13,38 @@ import "../styles/globals.css";
 // }
 
 function MyApp({ Component, pageProps }) {
-	// Use the layout defined at the page level, if available
-	const getLayout =
-		Component.getLayout ||
-		((page) => {
-			page;
-		});
+  // Use the layout defined at the page level, if available
+  const getLayout =
+    Component.getLayout ||
+    ((page) => {
+      page;
+    });
 
-	return (
-		<LocalizationProvider dateAdapter={AdapterDateFns}>
-			<StateProvider>
-				<SiteThemeProvider>
-					<CssBaseline />
-					<Layout>{getLayout(<Component {...pageProps} />)}</Layout>
-				</SiteThemeProvider>
-			</StateProvider>
-		</LocalizationProvider>
-	);
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StateProvider>
+        <SiteThemeProvider>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-RTV747GK9T"
+            strategy="afterInteractive"
+          ></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RTV747GK9T');
+       `}
+          </Script>
+
+          <CssBaseline />
+          <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
+        </SiteThemeProvider>
+      </StateProvider>
+    </LocalizationProvider>
+  );
 }
 
 export default MyApp;
