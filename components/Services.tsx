6cef97f039ad4dfdd8389d1bc03b9ec306a4ec58
Bernardo Munz <bernardo.munz@ivegroup.com.au>
2023-02-08 06:55:30 +1100

    add services

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,5 +1,6 @@
-import { Paper, Box, Alert } from "@mui/material";
-import { Wifi, SolarPower } from "@mui/icons-material";
+import { Paper, Box, Alert, List, ListItem, ListItemIcon } from "@mui/material";
+import Wifi from "@mui/icons-material/Wifi";
+import SolarPower from "@mui/icons-material/SolarPower";
 import CountertopsIcon from "@mui/icons-material/Countertops";
 import FlatwareIcon from "@mui/icons-material/Flatware";
 import KitchenIcon from "@mui/icons-material/Kitchen";
@@ -8,37 +9,70 @@ import DeckIcon from "@mui/icons-material/Deck";
 import RowingIcon from "@mui/icons-material/Rowing";
 import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
 
+const data = [
+  {
+    text: "Ahora la cabaña esta conectada a internet.",
+    icon: <Wifi />,
+    new: true,
+  },
+  {
+    text: "Tenemos Paneles solares para poder seguir teniendo luz en caso de  corte.",
+    icon: <SolarPower />,
+    new: true,
+  },
+  {
+    text: "La Cabaña cuenta con un living cocina-comedor equipada con utensilios y elementos de cocina para 2 ó 4 personas.",
+    icon: <FlatwareIcon />,
+    new: false,
+  },
+
+  {
+    text: "Heladera y Cocina convencional con Horno.",
+    icon: <KitchenIcon />,
+    new: false,
+  },
+
+  { text: "Blancos - Sabanas - Toallas", icon: <HotelIcon />, new: false },
+  {
+    text: "Cama matrimonial con vista al rio y fondo del parque en el piso superior.",
+    icon: <Wifi />,
+    new: false,
+  },
+  { text: "Deck con vista al río.", icon: <DeckIcon />, new: false },
+  {
+    text: "Una piragua a disposición para remar.",
+    icon: <RowingIcon />,
+    new: false,
+  },
+  {
+    text: "UParrilla y mesa en el jardín.",
+    icon: <OutdoorGrillIcon />,
+    new: false,
+  },
+  // { text: "Hamaca paraguaya", icon: <Wifi /> },
+];
+
 export const Services = () => (
   <Paper component="section">
     <Box p={5}>
-      <ul>
-        <li>
-          <Alert icon={<Wifi/>}>
-            
-            Ahora la cabaña esta conectada a internet.
-          </Alert>
-        </li>
-        <li>
-          <SolarPower />
-          Tenemos Paneles solares para poder seguir teniendo luz en caso de
-          corte.
-        </li>
-        <li>
-          La Cabaña cuenta con un living cocina-comedor equipada con utensilios
-          y elementos de cocina para 2 ó 4 personas.
-        </li>
-        <li> Cocina completa con Horno,</li>
-        <li>Heladera y Cocina convencional </li>
-        <li>Blancos - Sabanas - Toallas</li>
-        <li>
-          Cama matrimonial con vista al rio y fondo del parque en el piso
-          superior
-        </li>
-        <li>Deck con vista al río</li>
-        <li>Una piragua a disposición para remar</li>
-        <li>Parrilla y mesa en el jardín</li>
-        <li>Hamaca paraguaya</li>
-      </ul>
+      <List>
+        {data.map((item) => {
+          return <Item text={item.text} icon={item.icon} highlight={item.new}/>;
+        })}
+      </List>
     </Box>
   </Paper>
 );
+
+const Item = ({ text, icon, highlight }) => {
+  return (
+    <ListItem sx={{
+      color: highlight ? '#12f500': 'unset'
+    }} >
+      <ListItemIcon sx={{
+      color: highlight ? '#12f500': 'unset'
+    }}>{icon}</ListItemIcon>
+      {text}
+    </ListItem>
+  );
+};
