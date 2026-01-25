e6d534f847e772d26702668d83826d9136c08e02
Bernardo Munz <moonbe77@gmail.com>
2026-01-25 13:43:23 +1000

    remove all next files

--- components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./ImageListWrapper";
-export * from "./PageBanner";
-export * from "./HowToArribe";
-export * from "./Services";
-export * from "./About";
-export * from './ContactForm';
-export * from './Spacer'
