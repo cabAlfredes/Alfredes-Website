f9767060b3a510ecc7463b0e3a476000a14bafeb
Bernardo Munz <moonbe77@gmail.com>
2026-01-25 23:13:48 +1000

    migrate to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  output: "server",
+  output: "static",
   adapter: node({
     mode: "standalone",
   }),
